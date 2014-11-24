89a13f6f2049b7d981a4a579b2c6b91fa154b21b
Parsha Pourkhomami <parshap+git@gmail.com>
2014-11-23 16:44:43 -0800

    Check arguments and print helpful error message

--- bin/search-requires.js
+++ bin/search-requires.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 
+function isOptionEmpty(val) {
+  return ! val || ! val.length;
+}
+
 function readUsage() {
   return require("fs").createReadStream(
     require("path").join(__dirname, 'usage.txt')
@@ -23,6 +27,20 @@ if (argv.help) {
 
 var moduleToFind = argv.module;
 var entryPaths = argv._;
+
+if (isOptionEmpty(moduleToFind)) {
+  console.error("A target module must be given. Use the -m option.");
+  console.error();
+  readUsage().pipe(process.stderr);
+  return;
+}
+
+if (isOptionEmpty(entryPaths)) {
+  console.error("An entry file must be given.");
+  console.error();
+  readUsage().pipe(process.stderr);
+  return;
+}
 
 var finder = find(moduleToFind, entryPaths);
 finder.on("error", function(err) {
